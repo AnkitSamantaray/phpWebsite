c11eec0494e154e0c7a5b9fbf1018921f7f27ef2
Ankit Samantaray <44136101+AnkitSamantaray@users.noreply.github.com>
2019-03-12 18:34:17 +0000

    Initial Commit
    Php Blog Website

---
+++ phpProject/index.php
@@ -0,0 +1,23 @@
+<?php
+$title = "Home";
+$content = '<img src="Images/blog.png" class="imgLeft" />
+<h3>Title 1</h3>
+<p>
+    Blogging may be a fun hobby for Tumblr teens or WordPress writers, but it certainly isnt limited to personal pastimes. Today, blogging is one of the most popular ways to report on newsworthy topics.The most popular news blogs on the internet today have countless numbers of pages and receive millions of visits per month from people around the world. Have a look through a handful of the top blogs below and consider adding them to your favorite news reader to keep up with breaking news topics that interest.
+</p>
+
+<img src="Images/Blog1.png" class="imgRight"/>
+<h3>Title 2</h3>
+<p>
+    The Huffington Post specializes in reporting on news stories and events from practically every major category and subcategory you could imagine—including world news, entertainment, politics, business, style, and several others. Founded by Arianna Huffington, Kenneth Lerer, and Jonah Peretti in 2005, the blog was acquired by AOL in February 2011 for $315 million and has thousands of bloggers who contribute newsworthy written content on a wide range of topics.
+</p>
+
+<img src="Images/Blog2.png" class="imgLeft" />
+<h3>Title 3</h3>
+<p>
+   BuzzFeed is a trendy news blog that targets millennials. Focusing on social news and entertainment, the secret of BuzzFeeds success has a lot to do with the image-heavy listicles published on their platform and end up often going viral. Though founded in 2006, BuzzFeed really took off as a brand and news blog of its own in 2011 when it started publishing serious news and long-form journalism on topics like technology, business, politics, and more.
+</p>';
+
+include 'Template.php';
+?>
+
